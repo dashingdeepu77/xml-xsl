bc8401645b271d9a1145511b7e139593dd8313f9
Deepak <141410845+dashingdeepu77@users.noreply.github.com>
2023-08-27 23:08:51 +0530

    Add files via upload

---
+++ e-commerece.py
@@ -0,0 +1,31 @@
+from xml import etree
+
+# Load XML and XSL files
+
+xml = etree.parse("Labworks\Lab Ex5-XSL\ecommerce.xml")
+xsl = etree.parse("Labworks\Lab Ex5-XSL\ecommerce.xsl")
+xsd = etree.parse("Labworks\Lab Ex5-XSL\ecommerce.xsd") 
+
+
+# Apply XSL transformation
+transform = etree.XSLT(xsl)
+html = transform(xml)
+print(html)
+
+# Save transformed HTML to a file
+with open('Labworks\Lab Ex5-XSL\ecommerceparse.html', 'wb') as output_file:
+    output_file.write(etree.tostring(html, pretty_print=True))
+
+# Load XSD schema
+schema = etree.XMLSchema(xsd)
+
+
+# Validate transformed HTML against XSD schema
+validation_result = schema.validate(xml)
+
+if validation_result:
+    print("Validation successful!")
+else:
+    print("Validation errors:")
+    print("Validation Result :",validation_result)
+    print(schema.error_log)
